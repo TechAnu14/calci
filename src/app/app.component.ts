deeac6f6441eb8c9f5865b5429ba26481cc0fa0f
Anupriya <anuaji837@gmail.com>
2023-07-28 23:01:11 +0530

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calci';
+  input1=0;
+  input2=0;
+  result=0;
+  calculation(){
+
+  }
+  add()
+  {
+    this.result=this.input1+this.input2;
+  }
+  sub()
+  {
+    this.result=this.input1-this.input2;
+  }
+  mul()
+  {
+    this.result=this.input1*this.input2;
+  }
+  div()
+  {
+    this.result=this.input1/this.input2;
+  }
 }
